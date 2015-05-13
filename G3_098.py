46a2a9e622a812ab740f4d86b2ad72e72756e86c
Vaibhav Raj Gupta <gvaibhavraj2@gmail.com>
2015-05-14 01:29:40 +0530

    Space included

--- G3_098.py
+++ G3_098.py
@@ -42,10 +42,10 @@ def main():
 def generatequestion(j,k,m,n,z): 
 	builder = Builder(DEVELOPER_ID, QUESTION_CATEGORY)
 	questions_text = ["Fill the gap:","Put the correct integer in the gap to make the fractions equal:","Fill in the missing value:"]
-	builder.text(""+questions_text[random.randint(0,2)]+"\n", center=True,  color=RED_TEXT_COLOR,font_size=LARGE_FONT_SIZE)
+	builder.text(""+questions_text[random.randint(0,2)].upper()+"\n", center=True,  color=RED_TEXT_COLOR,font_size=LARGE_FONT_SIZE)
 	block_list=[]	   
 	block = builder.Block()
-	block.text(str(j)+"/"+str(k)+"=" , center=False, color=RED_TEXT_COLOR)
+	block.text(str(j)+"/"+str(k)+" = " , center=False, color=RED_TEXT_COLOR)
 	builder.break_line()
 	block.textbox(m,font_size=LARGE_FONT_SIZE)
 	block.text("/"+str(n),center=False, color=RED_TEXT_COLOR)
